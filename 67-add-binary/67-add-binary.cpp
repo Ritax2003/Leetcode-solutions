789e9b9214c8ca9c7a6d283b170a3454ccfe7aa2
RITABRATA DEY <82272821+Ritax2003@users.noreply.github.com>
2022-10-06 16:29:32 +0530

    Time: 6 ms (46.03%), Space: 6.2 MB (96.09%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -17,5 +17,15 @@ public:
             res+='1';
         reverse(res.begin(),res.end());
         return res;
+        /*string res="";
+        int carry = 0,i = a.size()-1,j = b.size()-1;
+        while(i>=0 || j>=0 || carry!=0){
+            int m = i<0 ? 0 : a[i--] - '0';
+            int n = j<0 ? 0 : b[j--] - '0';
+            res += (m+n+carry)%2 + '0';
+            carry = (m+n+carry)/2;
+        }
+        reverse(res.begin(),res.end());
+        return res;*/
     }
 };
