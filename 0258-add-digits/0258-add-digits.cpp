2abc32831fb5deb3a5fcaf11c50b949051dd03f2
RITABRATA DEY <82272821+Ritax2003@users.noreply.github.com>
2022-10-14 21:01:10 +0530

    Time: 7 ms (22.23%), Space: 5.8 MB (71.93%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-       int sum=0;
+      /* //iterative
+        int sum=0;
         while(num>9)
         {
             while(num)
@@ -12,7 +13,19 @@ public:
             num=sum;
             sum=0;
         }
-        return num;
-    } 
+        return num;*/
+    
+    //recursive
+    int sum=0;
+        while(num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else
+            return addDigits(sum);
+    }
     
 };
