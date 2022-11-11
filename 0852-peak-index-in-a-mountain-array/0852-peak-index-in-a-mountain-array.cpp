2394ee3e2a3aced4e612073831fa6581a7c1796f
RITABRATA DEY <82272821+Ritax2003@users.noreply.github.com>
2022-11-11 19:12:34 +0530

    Time: 395 ms (6.63%), Space: 59.7 MB (66.36%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        return max_element(arr.begin(),arr.end())-arr.begin();
+        //return max_element(arr.begin(),arr.end())-arr.begin();
+        int lo = 0, hi = arr.size()-1;
+        while(lo<hi){
+            int mi = lo + (hi - lo)/2;
+            if(arr[mi]<arr[mi+1]) lo = mi+1;
+            else hi = mi;
+        }
+        return lo;
     }
 };
