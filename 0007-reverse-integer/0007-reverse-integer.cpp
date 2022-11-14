86e81c6f76061119bacf8e1aa9b40540c530f9b4
RITABRATA DEY <82272821+Ritax2003@users.noreply.github.com>
2022-11-14 19:44:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.92%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -8,7 +8,7 @@ public:
         int m = x;
         while(m>0){
             int r = m%10;
-            if((rev>INT_MAX/10)||(rev<INT_MIN/10)) return 0;
+if((rev>INT_MAX/10)||(rev<INT_MIN/10)) return 0;
             rev = rev*10+r;
             m = m/10;
         }
