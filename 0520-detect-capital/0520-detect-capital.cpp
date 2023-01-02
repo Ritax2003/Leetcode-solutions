7272b7721fe983f744688ad9268f13727a4b1eeb
souraOP <sourasishmondal95@gmail.com>
2023-01-02 21:10:26 +0530

    Changed code for detectCapital

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-      int count=0;
-        for(int i=0;i<word.length();i++){
-            if(isupper(word[i])){
-                count++;
-            }
-        }
-        if(count==word.length() || count==0) return true;//case 1. "USA" , 2. "leetcode"
-        if(count==1 && isupper(word[0])) return true;//case 3. "Google"
-        else return false;  
+        int upperLetter = 0;
+		// count number of characters with upper case
+        for (auto x : word) upperLetter += isupper(x) ? 1 : 0;
+            // case 1: All letters in this word are capitals, like "USA".
+            // case 2: All letters in this word are not capitals, like "leetcode".
+            // case 3: Only the first letter in this word is capital, like "Google"
+        return upperLetter == word.size() || upperLetter == 0 ||(upperLetter == 1 && isupper(word.front()));
     }
 };
