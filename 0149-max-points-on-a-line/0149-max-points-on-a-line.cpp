1e0a4487602f86a4b8e1f2ee87acadce57d3b054
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-01-08 08:37:05 +0530

    Time: 35 ms (88.89%), Space: 12.6 MB (66.56%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -7,8 +7,10 @@ public:
         for(int i=0;i<n-1;i++){
             map<double,int> mp;
             for(int j=i+1;j<n;j++){
-                if(pt[j][1]==pt[i][1]) 
-                mp[-90]++;//i assume slope to avoid slope of 90 degree(we can say that is Perpendicular line) 
+                if(pt[j][1]==pt[i][1]){
+                mp[-90]++;
+                mp[90]++;
+                }//i assume slope to avoid slope of 90 degree(we can say that is Perpendicular line) 
                 else{ 
                     double x = (double)(pt[j][0]-pt[i][0])/(double)(pt[j][1]-pt[i][1]);
                     //storing the slop into map
