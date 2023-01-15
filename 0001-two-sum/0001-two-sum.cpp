1f5cd7621a49de70afd38c7004dd4307760372c4
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-01-15 08:40:31 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& numlist, int target) {
         vector<int>result;
-        for(int i =0;i<nums.size();i++){
-            for(int j =i+1;j<nums.size();j++){
+        for(int i =0;i<numlist.size();i++){
+            for(int j =i+1;j<numlist.size();j++){
                 if(nums[i]+nums[j]==target){
                     result.push_back(i);
                     result.push_back(j);
