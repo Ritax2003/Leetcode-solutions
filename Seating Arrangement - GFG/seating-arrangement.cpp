889780286920343d4144c3fa6f00256f1fbcc035
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-04-26 06:45:48 +0530

    Added solution - LeetHub

---
+++ Seating Arrangement - GFG/seating-arrangement.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool is_possible_to_get_seats(int n, int m, vector<int>& seats){
+       int c=0;
+        for(int i=0;i<m;i++){
+            int prev=i-1>=0?seats[i-1]:0;
+            int next=i+1<m?seats[i+1]:0;
+            if(!seats[i]&&prev==0&&next==0){
+                c++;
+                seats[i]=1;
+            }
+        }
+        return c>=n;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        vector<int> seats(m);
+        for (int i = 0; i < m; i++) {
+            cin >> seats[i];
+        }
+        Solution obj;
+        if (obj.is_possible_to_get_seats(n, m, seats)) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+}
+
+// } Driver Code Ends
