9907aa1a01d324a74f4e38eaa1693e347601f46e
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2023-10-14 12:00:33 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (22.54%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-       cout<< next_permutation(nums.begin(),nums.end());
+void nextPermutation(vector<int>& nums) {
+    int indx = -1;
+    int n = nums.size();
+    for(int i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            indx = i;
+            break;
+        }
     }
+    if(indx == -1){// when array is already sorted,it's the biggest number already
+        reverse(nums.begin(),nums.end());
+        return;
+    }
+    for(int i = n-1;i>indx;i--){
+        if(nums[i]>nums[indx]){
+            swap(nums[i],nums[indx]);
+            break;
+        }
+    }
+    reverse(nums.begin()+indx+1,nums.end());
+  }
 };
