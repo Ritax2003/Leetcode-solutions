e778522841b388ca273f698ea8f3f418403f9826
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-12 11:52:05 +0530

    Time: 2 ms (64.23%), Space: 7.2 MB (24.18%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+       unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int vowelsCount = 0;
+        int midIndex = s.length() / 2;
+
+        for (int i = 0; i < midIndex; i++) {
+            char charA = s[i];
+            char charB = s[midIndex + i];
+            if (vowels.count(charA)) vowelsCount++;
+            if (vowels.count(charB)) vowelsCount--;
+        }
+
+        return vowelsCount == 0;
+    }
+};
