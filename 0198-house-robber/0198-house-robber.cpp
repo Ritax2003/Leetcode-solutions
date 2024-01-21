d60bb122d6e61bb9b4adf85d89bc069b91c10c2a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-21 14:09:27 +0530

    Time: 4 ms (25.45%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
          int rob = 0;
         int norob = 0;
         for (int i = 0; i < nums.size(); i++) {
