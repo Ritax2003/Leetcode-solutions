2bc3532dbc95ca25ab625a3c346c3ca3c35177c9
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-01-22 09:02:47 +0530

    Time: 1728 ms (5.05%), Space: 23.8 MB (35.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
          int dup = -1, missing = -1;
         
         for (int i = 1; i <= nums.size(); i++) {
