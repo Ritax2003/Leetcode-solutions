fcbbfc85814fad3315876f5a152ec755848efe4e
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-02-07 11:56:14 +0530

    Time: 6 ms (95.59%), Space: 10.9 MB (21.89%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
+    
     string frequencySort(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<char,int>mp;
         vector<pair<int,char>>v;
         string ans = "";
