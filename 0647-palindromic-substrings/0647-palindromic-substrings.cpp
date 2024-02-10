4750c35cab03d09d73dbc0d1077fd6cc1828bfbd
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-02-10 10:43:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (74.15%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int n = s.size();
         int ans = 0;
         for(int i=0; i<n; i++){
             // odd length palindromes
-            int cnt = 0;
             int l = i, r = i;
             while(l >= 0 && r < n && s[l] == s[r]){
-                cnt++;
+                ans++;
                 l--, r++;
             }
-            ans += cnt;
 
             // even length palindromes
-            l = i, r = i+1, cnt = 0;
+            l = i, r = i+1;
             while(l >= 0 && r < n && s[l] == s[r]){
-                cnt++;
+                ans++;
                 l--, r++;
             }
-            ans += cnt;
         }
         return ans;
     }
