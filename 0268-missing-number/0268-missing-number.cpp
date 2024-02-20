3e509976a52864168858c54fd7775d24e213c87f
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-02-20 08:06:14 +0530

    Time: 6 ms (98.51%), Space: 20.5 MB (16.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int n = nums.size();
         int res = n*(n+1)/2;
         int i=0;
