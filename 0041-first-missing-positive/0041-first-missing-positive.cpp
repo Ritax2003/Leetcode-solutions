7242fe9b0cd6b3e0c3622cc86ab73c0d0cb4c6fa
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-03-27 07:04:03 +0530

    Time: 18 ms (99.49%), Space: 43.2 MB (99.42%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,17 +3,16 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         
-        int n = nums.size();
-        nums.push_back(0);
-    
-        for (int i = 0 ; i < n; i++)
-        {
-            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]])
-                swap(nums[i], nums[nums[i]]);
+       int n = nums.size();
+        for(int i =0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
+            }
         }
-        for (int i = 1; i <= n; ++i)
-            if (nums[i] != i)
-                return i;
-        return n + 1;
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
     }
 };
