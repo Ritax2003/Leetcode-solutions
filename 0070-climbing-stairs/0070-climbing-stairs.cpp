ae5f9de388fbb8649cdb3a60d4e7ef63f8d27124
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-07-22 12:46:38 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (11.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-       if(n==1){
-           return n;
-       }
-        int onestep=1;
-        int twostep =2;
+        if(n==1) return 1;
+        vector<int>dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        
         for(int i =3;i<=n;i++){
-            int third = twostep;
-            twostep = twostep+onestep;
-            onestep = third;
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        return twostep;
+        return dp[n];
     }
 };
