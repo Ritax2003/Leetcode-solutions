9cc24ed0326eacfdf95f2b93945837cf4b41e25a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-07-23 07:11:15 +0530

    Time: 3 ms (89.91%), Space: 15.4 MB (5.99%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        vector<pair<int,int>>vep;
+        vector<int>res;
+        for(auto x:nums){
+            mp[x]++;
+        }
+        for(auto x:mp){
+            int freq = x.second;
+            while(freq!=0){
+                vep.push_back({x.second,x.first});
+                freq--;
+            }
+        }
+        sort(vep.begin(),vep.end(),[](pair<int,int>&a,pair<int,int>&b){
+            if(a.first == b.first) return a>b;
+            return b>a;
+        });
+        for(auto x:vep){
+            res.push_back(x.second);
+        }
+        return res;
+    }
+};
