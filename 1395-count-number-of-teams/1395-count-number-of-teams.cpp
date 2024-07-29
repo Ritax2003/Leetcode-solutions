b50cd85c01c57d586004be78282af6fd9ad45546
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-07-29 16:05:40 +0530

    Time: 19 ms (94.73%), Space: 11.6 MB (51.74%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int ans = 0;
+        for(int i =0;i<n;i++){
+            int leftsmall = 0, leftbig = 0;
+            int rightsmall = 0,rightbig = 0;
+            
+            for(int j=i-1;j>=0;j--){
+                if(rating[i]>rating[j]) leftsmall+=1;
+                if(rating[i]<rating[j]) leftbig+=1;
+            }
+            for(int j=i+1;j<n;j++){
+                if(rating[i]>rating[j]) rightsmall+=1;
+                if(rating[i]<rating[j]) rightbig+=1;
+            }
+            
+            ans+=leftsmall*rightbig;
+            ans+=leftbig*rightsmall;
+        }
+        return ans;
+    }
+};
