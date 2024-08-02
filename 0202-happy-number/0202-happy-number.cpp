cd80ab7150b628bb6fe16edacd280eee8fb37685
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-02 10:46:01 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (84.04%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,36 +1,14 @@
 class Solution {
-public:/*
-    long square(int n){
-        int result =0;
-        while(n!=0){
-            int k = n%10;
-            result = result+(k*k);
-            n = n/10;
-        }
-        return result;
-    }
+public:
     bool isHappy(int n) {
-        int temp = n;
-        while(true){
-        if(square(temp)==89) return false;
-        if(square(temp)==1) return true;
-        temp = square(temp);
-        }
-    }
-    */
-    bool isHappy(int n) {
-        if(n == 1)
-            return true;
-        if(n > 1 && n <= 4)
-            return false;
-        
+        if(n==1) return true;
+        if(n>1 && n<=4) return false;
         int sum = 0;
-        while(n > 0) {    
-            int m = n % 10;    
-            sum = sum + (m * m);    
-            n = n / 10;
+        while(n>0){
+            int rem = n%10;
+            sum+=(rem*rem);
+            n/=10;
         }
-        
         return isHappy(sum);
     }
 };
