8fe1c6cd84785120b6dd2d51133709607eeaa435
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-24 09:41:23 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (59.56%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,43 +1,34 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
     string fractionAddition(string expression) {
         if (expression[0] != '-') expression = "+" + expression;
-        int numerator = 0, denominator = 1; // Initialize result as 0/1
+        int numerator = 0, denominator = 1;
         int n = expression.size();
-        int i = 0;
-        while (i < n) {
-            int sign = 1;
-            if (expression[i] == '-') sign = -1;
+        int i =0;
+        while(i<n){
+            int sign=1;
+            if(expression[i]=='-') sign = -1;
             i++;
-            // Read the numerator
-            int num = 0;
-            while (i < n && expression[i]!='/') {
-                num = num * 10 + (expression[i] - '0');
+            int num=0;
+            while(i<n && expression[i]!='/' ){
+                num = num*10 + (expression[i]-'0');
                 i++;
             }
-            num *= sign;
-            
-            // Skip the '/' character
-            i++;
-            
-            // Read the denominator
-            int denom = 0;
-            while (i < n && isdigit(expression[i])) {
-                denom = denom * 10 + (expression[i] - '0');
+            num = num*sign;
+            i++; //to ignore /
+            int deno=0;
+            while(i<n && isdigit(expression[i])){
+                deno = deno*10 + (expression[i]-'0');
                 i++;
             }
             
-            // Update for each frac
-            numerator = numerator * denom + num * denominator;
-            denominator *= denom;
+            numerator = numerator*deno + denominator*num;
+            denominator*=deno;
             
-            // Simplify the fraction
-            int gcd = abs(__gcd(numerator, denominator));
-            numerator /= gcd;
-            denominator /= gcd;
+            int gcd = abs(__gcd(numerator,denominator));
+            numerator/=gcd;
+            denominator/=gcd;
         }
-        
-        return to_string(numerator) + "/" + to_string(denominator);
+        return to_string(numerator)+'/'+to_string(denominator);
     }
 };
