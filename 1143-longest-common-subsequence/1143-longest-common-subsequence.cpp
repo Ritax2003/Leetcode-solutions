69911bd5d42f7cb6268f13a9da5959dd285001ac
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-30 09:34:34 +0530

    Time: 42 ms (18.97%), Space: 26.3 MB (8.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-   int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        if (i < 0 || j < 0) {    //reached at end of one of the strings.
-            return 0;
+    int fn(int i,int j, string &s1,string &s2, vector<vector<int>>&dp){
+        if(i<0||j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) dp[i][j]=1+fn(i-1,j-1,s1,s2,dp);
+        if(s1[i]!=s2[j]){
+            dp[i][j]=max(fn(i-1,j,s1,s2,dp),fn(i,j-1,s1,s2,dp));
+            
         }
-        
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        
-        if (s1[i] == s2[j]) {  // when both are correct are equal 
-            return dp[i][j] = 1 + f(i - 1, j - 1, s1, s2, dp);  // increment it and call recursion for remaining portion
-        }
-		// if not equal then
-        //remaining part of `s1` and `s2` (`f(i, j-1)`).
-		//remaining part of `s1` and `s2` (`f(i-1, j)`).
-       dp[i][j] = max(f(i, j - 1, s1, s2, dp), f(i - 1, j, s1, s2, dp));  
-       return dp[i][j];
+        return dp[i][j];
     }
-
-public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, s1, s2, dp);
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return fn(n-1,m-1,text1,text2,dp);
     }
 };
