e1555497e69c56c8e0c7ad5ad75ef77a6c517571
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-08-30 07:52:18 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (28.38%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int minlngth = INT_MAX;
+        int minlength = INT_MAX;
         string small;
-        for(string s : strs){
-            if(s.size()<minlngth){
-                minlngth = s.size();
+        for(string s:strs){
+            if(s.size() < minlength){
+                minlength = s.size();
                 small = s;
             }
         }
         
-        stack<char>st;
-        string ans="";
+        string ans;
+        stack<char>aleph;
         for(int i=0;i<small.size();i++){
             char current = small[i];
             bool allcommon = true;
-            for(int j=0;j<strs.size();j++){
-                if(strs[j][i]!= current){
+            for(int k=0;k<strs.size();k++){
+                if(strs[k][i]!=current){
                     allcommon = false;
                     break;
                 }
             }
-            
-            if(allcommon==true){
-                st.push(small[i]);
-            }
-            else{
-                break;
-            }
-        }
-        while(st.size()!=0){
-            ans+=st.top();
-            st.pop();
-        }
+                
+            if(allcommon) aleph.push(current);
+            else break;
+         }
+        
+       while(aleph.size()!=0){
+           ans+=aleph.top();
+           aleph.pop();
+       }
         reverse(ans.begin(),ans.end());
         return ans;
     }
