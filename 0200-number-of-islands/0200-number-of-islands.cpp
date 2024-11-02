2afc93b1a33926d2c28f1739d12c5c0fe9dd8f8a
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-11-02 11:56:43 +0530

    Time: 31 ms (42.55%), Space: 16.2 MB (66.70%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     void dfs(int r,int c,vector<vector<char>>& grid, vector<pair<int,int>>& dir){
-        int row = grid.size();
-        int col = grid[0].size();
+        int rows = grid.size();
+        int cols = grid[0].size();
         grid[r][c]='0';
-            for(pair<int,int>p :dir){
-                int nr = r+p.first, nc = c+p.second;
-                if(nr>=0 and nr <row && nc>=0 and nc<col and grid[nr][nc]=='1'){
-                    dfs(nr,nc,grid,dir);
-                }
+        
+        for(pair<int,int>p:dir){
+            int nr = r+p.first,nc = c+p.second;
+            if(nr>=0 and nr<rows and nc >=0 and nc<cols and grid[nr][nc]=='1'){
+                dfs(nr,nc,grid,dir);
             }
-            
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         if(grid.empty()) return 0;
         int rows = grid.size();
         int cols = grid[0].size();
-        int islandcount =0;
-        
-        vector<pair<int,int>>direction = {
-            {-1,0},//left
-            {1,0},//right
-            {0,-1},//down
-            {0,1}//up
+        int count=0;
+        vector<pair<int,int>>dir={
+            {-1,0},{1,0},{0,-1},{0,1}
         };
-        for(int row=0;row<rows;row++){
-            for(int col=0;col<cols;col++){
-                if(grid[row][col]=='1'){
-                    islandcount++;
-                    dfs(row,col,grid,direction);
+        
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c]=='1'){
+                    count++;
+                    dfs(r,c,grid,dir);
                 }
             }
         }
-        return islandcount;
+        return count;
     }
 };
