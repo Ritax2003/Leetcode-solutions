57792b33e39d3bf31c46fa2556030ad5ed833d9d
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-11-09 10:31:23 +0530

    Time: 824 ms (30.04%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -3,7 +3,7 @@ select s.user_id,
 case
     when s.user_id is not NULL and c.user_id is NULL then 0
     else round(
-        sum(c.user_id=s.user_id and c.action='confirmed')/count(c.user_id=s.user_id),2
+        sum(c.user_id=s.user_id and c.action='confirmed')/sum(c.user_id=s.user_id),2
     ) end as confirmation_rate
     
 from
