3dc215ac435e679a2537d53dbf780790b840d502
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2024-11-09 10:28:37 +0530

    Time: 954 ms (14.87%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 select s.user_id,
 case
-    when c.user_id is NULL then 0
+    when s.user_id is not NULL and c.user_id is NULL then 0
     else round(
         sum(c.user_id=s.user_id and c.action='confirmed')/count(c.user_id=s.user_id),2
     ) end as confirmation_rate
+    
 from
     Signups s left join Confirmations c
 on s.user_id = c.user_id
