d1b4f039b93e2085d9c947f876df1319396eed6c
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-01-19 22:54:36 +0530

    Time: 0 ms (100%), Space: 18.4 MB (74.21%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,19 +11,13 @@
  */
 class Solution {
 public:
-     bool chk(TreeNode* tree1, TreeNode* tree2){
-        if (tree1==NULL && tree2==NULL)return true;
-        if (!tree1 && tree2)return false;
-        if (!tree2 && tree1)return false;
-        if (tree1->val != tree2->val)return false;
-        return (chk(tree1->left,tree2->right) && chk(tree1->right,tree2->left));
+    bool isSymmetrich(TreeNode* n1, TreeNode* n2){
+        if(n1==NULL and n2==NULL) return true;
+        if(n1==NULL or n2==NULL) return false;
+        return n1->val == n2->val and isSymmetrich(n1->left,n2->right) and isSymmetrich(n1->right,n2->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if (root==NULL)return true;
-        if (root->right==NULL && root->left==NULL)return true;
-        if (root->left && root->right==NULL)return false;
-        if (root->right && root->left==NULL)return false;
-        // non null value for nodes
-        return chk(root->left,root->right);
+        if(root==NULL) return true;
+        return isSymmetrich(root->left,root->right);
     }
 };
