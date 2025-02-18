31290222d7847d70663e306b6209e608c5b19653
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-02-18 14:59:21 +0530

    Time: 476 ms (5.05%), Space: 41.9 MB (5.29%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-    vector<int> generatePrefixSum(vector<int>& a) {
-        int n = a.size();
-        vector<int> pref(n);
-        pref[0] = a[0];
-        for (int i = 1; i < n; i++) pref[i] = pref[i - 1] + a[i];
-        return pref;
-    }
-
     int subarraysDivByK(vector<int>& nums, int k) {
-        // generate prefix sum
-        vector<int> pref = generatePrefixSum(nums);
-        // store the modular cnt
-        vector<int> m(k);
-        // base value
-        m[0] = 1;
-        // init ans
-        int ans = 0;
-        // iterate each sum
-        for (auto& x : pref) {
-            // if x is negative, turn it to positive modular equivalent
-            if (x < 0) x = (x % k + k) % k;
-            // we need to find pairs such that (pref[j] - pref[i]) % k == 0
-            // hence, we look for pref[j] % k == pref[i] % k
-            // why ..? 
-            // pref[j] = a * k + x
-            // pref[i] = b * k + y
-            // pref[j] - pref[i] = (a * k + x) - (b * k + y)
-            // pref[j] - pref[i] = k * (a - b) + (x - y)
-            // -> (pref[j] - pref[i]) % k == 0 if & only if (x - y) == 0
-            m[x % k]++;
+        unordered_map<int,vector<int>>mp;
+        mp[0]={-1};
+        int sum=0;
+        int cnt=0;
+        vector<vector<int>>res;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            int rem = (((sum%k)+k)%k);
+            if(mp.find(rem)!=mp.end()){
+                for(int start:mp[rem]){
+                    //vector<int>subarray(nums.begin()+start+1,nums.begin()+i+1);
+                    //res.push_back(subarray);
+                    cnt++;
+                }
+            }
+            mp[rem].push_back(i);
         }
-        // n choose 2
-        for (auto& x : m) ans += x * (x - 1) / 2;
-        return ans;
+
+        return cnt;
     }
 };
