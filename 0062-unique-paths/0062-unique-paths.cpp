4702f9d38c005c539bb4e98bccbe64e765cf797b
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-02-19 07:50:47 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int countpaths(int i,int j,int m , int n, vector<vector<int>> &dp){
-        if(i== m-1 and j == n-1) return true;
-        if(i >= m or j>=n) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = countpaths(i+1,j,m,n,dp);
-        int right = countpaths(i,j+1,m,n,dp);
-        dp[i][j]= down+right;
-        return dp[i][j];
+    int countpaths(int i,int j,int m,int n, vector<vector<int>>&grid){
+        if(i>=m or j>=n) return false;
+        if(i==m-1 and j==n-1) return true;
+        if(grid[i][j]!=-1) return grid[i][j];
+
+        int down = countpaths(i+1,j,m,n,grid);
+        int right = countpaths(i,j+1,m,n,grid);
+
+        grid[i][j]= down+right;
+        return grid[i][j];
+
     }
     int uniquePaths(int m, int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        vector<vector<int>>dp(m+1, vector<int>(n+1,-1));
-       
-        return countpaths(0,0,m,n,dp);
+        vector<vector<int>>grid(m+1,vector<int>(n+1,-1));
+        return countpaths(0,0,m,n,grid);
     }
 };
