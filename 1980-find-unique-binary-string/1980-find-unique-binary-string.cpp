74b13f530d663fb29089796ea80b48d681219bd8
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-02-20 12:37:24 +0530

    Time: 1 ms (32.35%), Space: 12.6 MB (69.88%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        string ans = string(nums.size(),'0'); //starting number with all 0.
-        if(find(nums.begin(),nums.end(), ans) ==nums.end()) return ans;
-
+        string ans = string(nums.size(),'0');
+        if(find(nums.begin(),nums.end(),ans)==nums.end()) return ans;
         for(int i=0;i<nums.size();i++){
-            ans[i]='1'; // flip 1 bit at a time
-            if(find(nums.begin(),nums.end(), ans)==nums.end()) return ans;
-            ans[i]='0'; // revert;
+            ans[i]='1';
+            if(find(nums.begin(),nums.end(),ans)==nums.end()) return ans;
+            ans[i]='0';
         }
         return ans;
     }
