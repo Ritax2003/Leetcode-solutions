1e6676ab7affdd271ed37003d1cbd5f70987031f
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-01 21:07:14 +0530

    Time: 3 ms (76.27%), Space: 14.3 MB (34.3%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        if(numRows <= 1) return s;
-        int j=0;
-        
-        vector<string>v(numRows,"");
+    string convert(string s, int n) {
+        if(n<=1) return s;
+        int j = 0;
+        vector<string>v(n,"");
         int dir = -1;
-        for(int i=0;i<s.length();i++){
-            if(j==0||j == numRows-1) dir*=-1;
+        for(int i =0;i<s.length();i++){
+            if(j==0||j==n-1) dir*=-1;
             v[j]+=s[i];
-            if(dir == 1) j++;
+            if(dir==1) j++;
             else j--;
         }
-         string res;
-
-    for(auto &it : v) res += it; 
-
-    return res;
+        string res;
+        for(auto it : v) res+=it;
+        return res;
     }
 };
