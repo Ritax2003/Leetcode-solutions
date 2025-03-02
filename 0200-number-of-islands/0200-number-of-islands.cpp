2f16fa4f6ce4e89f33b9fe240ae882ce6b2b450f
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-02 19:40:02 +0530

    Time: 46 ms (14.54%), Space: 22.4 MB (36.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    void dfs(int r,int c ,vector<vector<char>>& grid, vector<pair<int,int>>dir){
-        int rows = grid.size();
-        int cols = grid[0].size();
-        grid[r][c]='0';
-        
-        for(pair<int,int>p : dir){
-            int nr = r+p.first;
-            int nc = c+p.second;
-            if(nr>=0 and nr<rows and nc>=0 and nc<cols and grid[nr][nc]=='1'){
-                dfs(nr,nc,grid,dir);
-            }
+void dfs(int r,int c,vector<vector<char>>& grid, vector<pair<int,int>> dir){
+    int rows = grid.size();
+    int cols = grid[0].size();
+    grid[r][c]='0';
+
+    for(pair<int,int>p:dir){
+        int nr = r+p.first;
+        int nc = c+p.second;
+        if(nr>=0 and nr<rows and nc>=0 and nc<cols and grid[nr][nc]=='1'){
+            dfs(nr,nc,grid,dir);
         }
     }
+}
     int numIslands(vector<vector<char>>& grid) {
         if(grid.size()==0) return 0;
         int rows = grid.size();
         int cols = grid[0].size();
         int count = 0;
-        vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
-        
-        
-        for(int r=0;r<rows;r++){
-            for(int c =0;c<cols;c++){
-                if(grid[r][c]=='1'){
+        vector<pair<int,int>>dir={{-1,0},{1,0},{0,1},{0,-1}};
+
+        for(int i =0;i<rows;i++){
+            for(int j =0;j<cols;j++){
+                if(grid[i][j]=='1'){
                     count++;
-                    dfs(r,c,grid,dir);
+                    dfs(i,j,grid,dir);
                 }
             }
         }
