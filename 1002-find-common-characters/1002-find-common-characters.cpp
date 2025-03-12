c829e65a820a457cf27ac25ce00afa14162a83c4
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-12 11:31:31 +0530

    Time: 0 ms (100%), Space: 12.7 MB (36.6%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        vector<int> minFreq(26,INT_MAX); // Store min frequency of each character
-
-        for (string& word : words) {
-            vector<int> freq(26, 0);
-            for (char ch : word) {
-                freq[ch -'a']++; // Count frequency of each character in the word
+        vector<int>minFreq(26,INT_MAX);
+        for(string word : words){
+            vector<int>freq(26,0);
+            for(char c : word){
+                freq[c-'a']++;
             }
-            for (int i = 0; i < 26; i++) {
-                minFreq[i] = min(minFreq[i],freq[i]); // Update min frequency across words
-            }
-        }
-
-        vector<string> res;
-        for (int i = 0; i < 26; i++) {
-            while (minFreq[i]-- > 0) {
-                res.push_back(string(1, i + 'a')); // Add character to result
+            for(int i=0;i<26;i++){
+                minFreq[i]=min(minFreq[i],freq[i]);
             }
         }
-
+        vector<string>res;
+        for(int i =0;i<26;i++){
+            while(minFreq[i]-->0){
+                res.push_back(string(1,i+'a'));
+            }
+        }
         return res;
     }
 };
