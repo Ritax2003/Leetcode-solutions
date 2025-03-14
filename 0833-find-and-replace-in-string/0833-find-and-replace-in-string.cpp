e0b853ed7cbed4c6ee2a91f37bef82d6431c2982
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-14 21:49:13 +0530

    Time: 0 ms (100%), Space: 14.3 MB (50.72%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        unordered_map<int,pair<string,string>>replacements;
-        for(int i=0;i<indices.size();i++){
+        unordered_map<int,pair<string,string>>mp;
+        for(int i =0;i<indices.size();i++){
             int indx = indices[i];
-            string src = sources[i], tgt = targets[i];
-            if(s.substr(indx,src.size())==src){
-                replacements[indx]={src,tgt};
+            string src = sources[i] , tgt = targets[i];
+            if(s.substr(indx,src.length()) == src){
+                mp[indx] = {src,tgt};
             }
         }
 
-        string res="";
-        int i = 0;
-        while(i<s.length()){
-            if(replacements.find(i)!=replacements.end()){
-                res+=replacements[i].second;
-                i+=replacements[i].first.length();
+        string res = "";
+        int i =0;
+        while(i<s.size()){
+            if(mp.find(i)!=mp.end()){
+                res+=mp[i].second;
+                string source = mp[i].first;
+                i+=source.length();
             }
             else{
                 res+=s[i];
