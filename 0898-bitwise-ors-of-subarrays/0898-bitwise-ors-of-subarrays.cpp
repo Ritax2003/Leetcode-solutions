ac1dd69a07807e7cc946d3e43efc9b74ab1e2069
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-14 16:45:58 +0530

    Time: 1058 ms (41.43%), Space: 339.9 MB (17.86%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
@@ -3,19 +3,15 @@ public:
     int subarrayBitwiseORs(vector<int>& arr) {
         unordered_set<int>uniqueOR;
         unordered_set<int>prevOR;
+        for(int a : arr){
+            unordered_set<int> newOR;
+            newOR.insert(a);
 
-        for(int num:arr){
-            unordered_set<int>newOR;
-            newOR.insert(num);
-
-            for(int val:prevOR){
-                newOR.insert(val|num);
-            }
-
-            prevOR=newOR;
+            for(int num : prevOR) newOR.insert(a|num);
+            prevOR = newOR;
             uniqueOR.insert(prevOR.begin(),prevOR.end());
-        }
 
+        }
         return uniqueOR.size();
     }
 };
