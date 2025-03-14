724c27da63ae8ab0bbaf089451fa2603074c59ef
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-14 23:01:06 +0530

    Time: 4 ms (53.18%), Space: 112.1 MB (26.85%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        long long mod = 1e9+7;
-        int odd_count= 0;
-        int even_count = 1;
-        int sum=0,res=0;
-        for(int num: arr){
-            sum+=num;
-
+        long long k = 1e9+7;
+        int odd_cnt = 0, even_cnt = 1;
+        int sum =0,res=0;
+        for(int a:arr){
+            sum+=a;
             if(sum%2==1){
-                res= (res+even_count)%mod;
-                odd_count++;
+                res=(res+even_cnt)%k;
+                odd_cnt++;
             }
             else{
-                res=(res+odd_count)%mod;
-                even_count++;
+                res=(res+odd_cnt)%k;
+                even_cnt++;
             }
         }
         return res;
