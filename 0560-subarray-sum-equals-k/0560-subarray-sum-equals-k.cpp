699637502f3c28102fd2a9145685a5f5a3a13df8
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-14 18:39:04 +0530

    Time: 338 ms (26.09%), Space: 59.8 MB (5.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,vector<int>>mp;
-        mp[0]={-1};
-        int cnt =0;
-        vector<vector<int>>res;
-        int prefixsum=0;
+        unordered_map<int,vector<int>>prefixmap;
+        vector<vector<int>>ans;
+        prefixmap[0]={-1};
+        int prefixsum =0;
+        int cnt = 0;
         for(int i=0;i<nums.size();i++){
             prefixsum+=nums[i];
-            if(mp.find(prefixsum-k)!=mp.end()){
-                for(int start: mp[prefixsum-k]){
-                    //vectpr<int>subarray(nums.begin()+start+1,nums.begin()+i+1);
-                    //res.push_back(subarray);
+            if(prefixmap.find(prefixsum-k)!=prefixmap.end()){
+                for(auto start : prefixmap[prefixsum-k]){
+                    //vector<int>subarray(nums.begin()+start+1,nums.end()+i+1);
+                    //ans.push_back(subarray);
                     cnt++;
                 }
             }
-            mp[prefixsum].push_back(i);
+            prefixmap[prefixsum].push_back(i);
         }
         return cnt;
     }
