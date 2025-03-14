686eace0c4ccfacaff4692b0b88f1d46b8262a73
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-03-14 13:02:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int helper(int i, int j,int m,int n,vector<vector<int>>&grid){
-        if(i>=m or j>=n) return false;
+    int helper(vector<vector<int>>&dp,int i,int j,int m,int n){
         if(i==m-1 and j==n-1) return true;
-        if(grid[i][j]!=0) return grid[i][j];
-        int right = helper(i+1,j,m,n,grid);
-        int down = helper(i,j+1,m,n,grid);
-        grid[i][j]= right+down;
-        return grid[i][j];
+        if(i>=m or j>=n) return false;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right = helper(dp,i+1,j,m,n);
+        int down = helper(dp,i,j+1,m,n);
+        dp[i][j]=right+down;
+        return dp[i][j];
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>grid(m+1,vector<int>(n+1,0));
-        return helper(0,0,m,n,grid);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return helper(dp,0,0,m,n);
     }
 };
