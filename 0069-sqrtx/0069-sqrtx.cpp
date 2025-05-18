a560ef38e0991c6858f13d5a2618eae44fe21c14
Ritabrata Dey <82272821+Ritax2003@users.noreply.github.com>
2025-05-18 05:42:01 +0530

    Time: 9 ms (13.18%), Space: 6 MB (100%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long y = x;
-        while(y*y>x){
-            y = (y +x/y)/2;
+      /*  double t = x;
+       while (t * t - x >= 1) 
+           t = 0.5 * (t + x /t);
+       return t;*/
+        int lo = 0,hi = x, ans;
+        while(lo<=hi){
+        long long int mid = lo +(hi - lo)/2;
+        if(mid*mid == x) {
+            ans = mid;
+            break;
         }
-        return y;
+        else if(mid*mid<x){ lo = mid + 1;}
+        else {hi = mid-1;}
+        }
+        return ans;
     }
 };
